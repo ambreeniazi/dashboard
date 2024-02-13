47678ab8fff25a920da6ec77b1346e19cd5186ef
Ambreen786928 <ambreenk255@gmail.com>
2024-02-13 10:57:09 +0500

    initialize:first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+        <ol>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ol>
+    </div>
+    </>
+  );
+}
+
+export default Navbar;
